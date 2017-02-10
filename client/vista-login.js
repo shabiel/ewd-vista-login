99a0e02df8f1380741eb5b7e32986a8465d08760
Alexis Carlson <whatisthehumanspirit@gmail.com>
2017-02-09 17:31:14 -0800

    Make server side of autocomplete dynamic.

--- client/vista-login.js
+++ client/vista-login.js
@@ -619,7 +619,6 @@ clientMethods.getUsers = function(EWD) {
   $.widget( "ui.autocomplete", $.ui.autocomplete, {
     _renderItem: function(ul, item) {
       // Grab fields data from autocomplete element
-      // Stored with each request
       let fields = this.element.data('fields');
       
       let html = '';
@@ -639,7 +638,6 @@ clientMethods.getUsers = function(EWD) {
     options: {
       select: function(event, ui) {
         // Grab fields data from autocomplete element
-        // Stored with each request
         let fields = $(this).data('fields');
         
         $(event.target).data('record', ui.item);
@@ -660,14 +658,20 @@ clientMethods.getUsers = function(EWD) {
       
       let messageObj = {
         service: 'ewd-vista-login',
-        type: 'getUsers',
-        params: { query: request.term }
+        type: 'listDic',
+        params: {
+          query: {
+            file: '200',
+            fields: ['.01', '1', '4', '5'],
+            string: request.term,
+            quantity: 8
+          }
+        }
       };
       EWD.send(messageObj, function(responseObj) {
         let results = responseObj.message.results;
                 
-        // Attach file & fields data to the element so the menu can include it
-        // dynamically
+        // Attach file & fields data to the element so the menu can use it
         if (!element.data('fields')) {
           element.data('file', results.file);
           element.data('fields', results.fields);
